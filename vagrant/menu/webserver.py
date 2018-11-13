c1e63b35fc9f2f3d6fd06af7af5500066175385a
Virgil Anderson <Virgil.Anderson@gmail.com>
2018-11-13 14:07:12 -0700

    added do_POST

--- vagrant/menu/webserver.py
+++ vagrant/menu/webserver.py
@@ -34,6 +34,18 @@ class webserverHandler(BaseHTTPRequestHandler):
             self.send_response(301)
             self.end_headers()
 
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            if ctype = 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('message')
+
+            output = ''
+            output += "<html><body>"
+            output += " <h2> Okay, how about this: </h2>"
+            output += "<h1> % </h1>" % messagecontent[0]
+
+            output += "<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like to say?</h2><input name='message' type='text'><input type='submit' value='Submit'></form>"
+
         except:
 
 def main():
