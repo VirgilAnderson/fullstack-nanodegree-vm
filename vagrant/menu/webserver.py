4a74f412381f9e1537e67d957c85e2a137ff8191
Virgil Anderson <Virgil.Anderson@gmail.com>
2018-11-13 12:30:24 -0700

    added the try and exception functions within div main()

--- vagrant/menu/webserver.py
+++ vagrant/menu/webserver.py
@@ -1,1 +1,15 @@
-from BastHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+def main():
+    try:
+        port = 8080
+        server = HTTPServer(('', port), webserverHandler)
+        print "Web server running on port %s" % port
+        server.serve_forever()
+
+    except KeyboardInterrupt:
+        print "^C entered, stopping web server..."
+        server.socket.close()
+
+if __name-- == '__main__':
+    main()
